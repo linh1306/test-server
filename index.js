e8bed45b32e2b381bcfba87a31d1de105f782894
linh1306 <nguyenlinh13602@gmail.com>
2023-12-23 12:38:58 +0700

    fix api

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require('express');
+const axios = require('axios');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-// Sử dụng middleware để parse dữ liệu JSON từ yêu cầu POST
 app.use(bodyParser.json());
 
-// Định nghĩa route POST để xử lý dữ liệu JSON
-app.post('/test', (req, res) => {
-  const inputData = req.body; // Dữ liệu JSON từ người dùng
+app.post('/', async (req, res) => {
+  const inputData = req.body;
+  const data = {
+    "key": "e3c9be8a60dfcddf028d8261b265114b",
+    "token": "ATTA202fea308de0c7eb62c8b711a64441deeea454c02e8b01954e261f9f56908a5a38DF27DA",
+    "idList": "6581457631dc4371ad36bb24",
+    "name": inputData.name,
+    "desc": inputData.desc,
+    "fileSource": inputData.fileSource
+  }
   res.json(inputData);
-});
-// Định nghĩa một route đơn giản
-app.get('/', (req, res) => {
-  res.send('Hello, Render!');
+
+  axios.post("https://api.trello.com/1/cards", data)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(error => {
+      console.error('Error:', error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
 });
 
-// Chạy server trên cổng được cấu hình hoặc cổng 3000 nếu không có cấu hình
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
