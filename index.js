53b00af7b47d7db8d6dc7d6ee080f367c8665a26
linh.nv <linh.nv@tinasoft.com.vn>
2024-06-16 13:17:00 +0700

    p

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
+// Import các module cần thiết
 const express = require('express');
-const axios = require('axios');
-const multer = require('multer');
-const FormData = require('form-data');
+const http = require('http');
+const socketIo = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
-
 app.use(cors());
+const server = http.createServer(app);
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-app.post('/', upload.single('fileSource'), async (req, res) => {
-  try {
-    const file = req.file;
-    const data = req.body;
-    const trelloForm = new FormData();
-    Object.entries(data).forEach(([key, value]) => {
-      trelloForm.append(key, value);
-    });
-    if (file) {
-      trelloForm.append('fileSource', file.buffer, { filename: file.originalname });
-    }
-    const trelloResponse = await axios.post("https://api.trello.com/1/cards", trelloForm, {
-      headers: {
-        ...trelloForm.getHeaders(),
-      },
-    });
-    const trelloResult = trelloResponse.data;
-    res.json(trelloResult);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('cn', (data) => {
+    console.log('Message from client:', data);
+    io.emit('chat', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+  socket.on('createRoom', (roomName) => {
+    socket.join(roomName);
+    console.log(`Socket ${io.id} joined room ${roomName}`);
+    socket.emit('roomCreated', roomName);
+  });
+
+  socket.on('chatMessage', (data) => {
+    io.to(data.room).emit('chatMessage', data.message);
+  });
+});
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
 });
